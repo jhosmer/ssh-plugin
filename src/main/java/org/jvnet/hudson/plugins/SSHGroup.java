dbc5266fc949a89b99f851b045ee06ed1a98b135
Jonathan <tetris24@gmail.com>
2014-12-03 17:01:07 -0500

    Update SSHGroup.java

--- src/main/java/org/jvnet/hudson/plugins/SSHGroup.java
+++ src/main/java/org/jvnet/hudson/plugins/SSHGroup.java
@@ -1,11 +1,5 @@
 package org.jvnet.hudson.plugins;
 
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import com.jcraft.jsch.UserInfo;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
@@ -14,19 +8,18 @@ import java.util.logging.Logger;
 public class SSHGroup {
     SSHSite[] sites;
 
-	public static final Logger LOGGER = Logger.getLogger(SSHGroup.class.getName());
+    public static final Logger LOGGER = Logger.getLogger(SSHGroup.class.getName());
 
-	public SSHGroup() {
+    public SSHGroup() {
 
-	}
+    }
 
-	public SSHGroup(SSHSite[] sites) {
-	  this.sites = sites;
-	}
+    public SSHGroup(SSHSite[] sites) {
+        this.sites = sites;
+    }
 
-	@Override
-	public String toString() {
-		return "SSHGroup [sites=" + this.sites + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "SSHGroup [sites=" + this.sites + "]";
+    }
 }
